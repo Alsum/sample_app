60b209f473b33c88e7250670ac720d7e859f3f7a
sprint <sprint@sprint-pc1.(none)>
2013-02-11 14:01:06 +0200

    users microposts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'cc54bfc3c4fcdd764057b4d3a58dba5f58f7142c1165491e0cea1cb951e4975059e1085cf4ecef7698a5a3a73119403bf2dd8e0060b4e3d19114eeb18783d964'
